282f11d84966d6b01d88534408fc5529d62b01f1
vijay086 <158210280+vijay086@users.noreply.github.com>
2024-02-05 22:35:18 +0530

    Update push-pop.c

--- push-pop.c
+++ push-pop.c
@@ -1,95 +1,75 @@
 #include<stdio.h>
-#include<conio.h>
+#include<stdlib.h>
+
 #define MAX 5
 
-int a[MAX] , top = -1 ;
+int a[MAX], top = -1;
+
 void push();
 void pop();
-void disp();
+void display();
 
-int main()
-{
+int main() {
     int ch;
-    printf("1. PUSH or Insert \n");
-    printf("2. POP or Delete  \n");
-    printf("3. Display \n");
-    printf("4. End the program \n");
-    
-    while(1)
-    {
-        printf("\n Enter choice : ");
-        scanf("D",ch);
-        
-        switch(ch)
-        {
+
+    printf("1. PUSH or Insert\n");
+    printf("2. POP or Delete\n");
+    printf("3. Display\n");
+    printf("4. End the program\n");
+
+    while (1) {
+        printf("\nEnter choice: ");
+        scanf("%d", &ch);
+
+        switch (ch) {
             case 1:
-            {
                 push();
                 break;
-            }
             case 2:
-            {
                 pop();
                 break;
-            }
             case 3:
-            {
                 display();
                 break;
-            }
             case 4:
-            {
                 exit(0);
-            }
             default:
-            {
-                printf("Wrong choice");
-            }
+                printf("Wrong choice\n");
         }
-        break;
     }
+
+    return 0;
 }
-void push()
-{
+
+void push() {
     int data;
-    if(top==MAX-1)
-    {
-        printf("\n Overflow or Stack is full ");
-    }
-    else
-    {
-        printf("Enter Elements to be Pushed");
-        scanf("D",&data);
+    if (top == MAX - 1) {
+        printf("\nOverflow or Stack is full\n");
+    } else {
+        printf("Enter Element to be Pushed: ");
+        scanf("%d", &data);
         top++;
-        a[top]=data;
+        a[top] = data;
     }
 }
-void pop()
-{
-    if(top==-1)
-    {
-        printf("Underflow or Stack is Empty");
-    }
-    else
-    {
-        printf("Popped Element  : %d",a[top]);
+
+void pop() {
+    if (top == -1) {
+        printf("Underflow or Stack is Empty\n");
+    } else {
+        printf("Popped Element: %d\n", a[top]);
         top--;
     }
 }
-void display()
-{
+
+void display() {
     int i;
-    if(top>=0)
-    {
-        printf("Elements :");
-        for(i=top;i>=0;i--)
-        {
-            printf("\n d",a[i]);
+    if (top >= 0) {
+        printf("Elements:\n");
+        for (i = top; i >= 0; i--) {
+            printf("%d\n", a[i]);
         }
-        
-    }
-    else
-    {
-        printf("The Stack is Empty");
+    } else {
+        printf("The Stack is Empty\n");
     }
 }
